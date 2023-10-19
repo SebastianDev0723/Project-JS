dc4711da6798834a5c0d47b725a0ea1b9def3c18
Stoian Sebastian <stoyan_seby@yahoo.com>
2023-10-19 20:42:02 +0300

    update

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -19,12 +19,20 @@ function calculateScore() {
     const resultMessage = `Ai obținut ${correctCount} puncte din 10.<br><br>` +
     `Răspunsuri corecte: ${correctCount}<br>` +
     `Răspunsuri greșite: ${wrongCount}`;
-
+    
     document.getElementById("result").innerHTML = resultMessage;
 }
 
 function resetForm() {
     document.getElementById("quizForm").reset();
-
     document.getElementById("result").innerHTML = "";
 }
+
+function startQuiz() {
+    let quizForm = document.getElementById("quizForm");
+    quizForm.classList.remove("d-none");
+}
+
+document.getElementById("calculateButton").addEventListener("click", calculateScore);
+document.getElementById("resetButton").addEventListener("click", resetForm);
+document.getElementById("startTimer").addEventListener("click", startQuiz);
--- javascript/timer.js
+++ javascript/timer.js
@@ -1,26 +1,29 @@
 let timer;
 const element = document.getElementById("timer");
 let seconds = 0;
+let isPaused = false;
 
 const startTimer = () => {
     if (!timer) {
         timer = setInterval(() => {
+            if (isPaused) {
+                return;
+            }
             seconds++;
             const minutes = Math.floor(seconds / 60);
             const remainingSeconds = seconds % 60;
-            element.innerHTML = pad(minutes) + ":" + pad(remainingSeconds);
-        }, 1000);
+            element.innerHTML = pad(minutes) + ":" + pad(remainingSeconds); 
+        }, 1000); 
     }
 };
-
-const resetTimer = () => {
-    clearInterval(timer);
-     timer = null;
-     seconds = 0;
-     element.innerHTML = '00:00';
-};
+const pauseTimer = () => {
+    isPaused = true ;
+}
 
 const pad = val => {
     const valString = val + "";
     return valString.length < 2 ? "0" + valString : valString;
 };
+
+document.getElementById("startTimer").addEventListener("click", startTimer)
+document.getElementById("calculateButton").addEventListener("click", pauseTimer);
